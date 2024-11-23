d7e533733560bc5b39340832835b17f80663ec56
Dejan Pirc <ddeanp@gmail.com>
2024-11-23 19:15:25 +0100

    ADD: add updateScore_liveMatch_matchScoreIsUpdated test

--- src/test/java/si/dejan/scoreboard/ScoreboardTest.java
+++ src/test/java/si/dejan/scoreboard/ScoreboardTest.java
@@ -7,35 +7,49 @@ import org.junit.Test;
 public class ScoreboardTest {
 
     @Test
-    public void startMatch_startNewMatch_scoreIsZero(){
+    public void startMatch_startNewMatch_scoreIsZero() {
         Team homeTeam = new Team("Mexico");
         Team awayTeam = new Team("Canada");
         Match match = new Match(homeTeam, awayTeam);
         Scoreboard scoreboard = new Scoreboard();
-        
+
         scoreboard.startMatch(match);
 
-        assert(match.getHomeTeam().getScore() == 0);
-        assert(match.getAwayTeam().getScore() == 0);
+        assert (match.getHomeTeam().getScore() == 0);
+        assert (match.getAwayTeam().getScore() == 0);
     }
 
     @Test
-    public void startMatch_matchIsNotSet_throwsException(){
+    public void startMatch_matchIsNotSet_throwsException() {
         Scoreboard scoreboard = new Scoreboard();
-        
+
         Exception exception = assertThrows(Exception.class, () -> scoreboard.startMatch(null));
         assertEquals("Match must be set!", exception.getMessage());
     }
 
     @Test
-    public void startMatch_teamIsAlreadyPlayingInOtherMatch_throwsException(){
+    public void startMatch_teamIsAlreadyPlayingInOtherMatch_throwsException() {
         Team homeTeam = new Team("Mexico");
         Team awayTeam = new Team("Canada");
         Match match = new Match(homeTeam, awayTeam);
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startMatch(match);
-        
+
         Exception exception = assertThrows(Exception.class, () -> scoreboard.startMatch(match));
         assertEquals("Invalid match: team Mexico is already playing in other match!", exception.getMessage());
+    }
+
+    @Test
+    public void updateScore_liveMatch_matchScoreIsUpdated() {
+        Team homeTeam = new Team("Mexico");
+        Team awayTeam = new Team("Canada");
+        Match match = new Match(homeTeam, awayTeam);
+        Scoreboard scoreboard = new Scoreboard();
+        scoreboard.startMatch(match);
+
+        scoreboard.updateScore(match, 1, 2);
+
+        assert (match.getHomeTeam().getScore() == 1);
+        assert (match.getAwayTeam().getScore() == 2);
     }
 }
