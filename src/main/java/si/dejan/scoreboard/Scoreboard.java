0f71a3f9191eb24f0caa20526876fe273c3d346f
Dejan Pirc <ddeanp@gmail.com>
2024-11-23 17:39:22 +0100

    UPDATE: add null check to startMatch

--- src/main/java/si/dejan/scoreboard/Scoreboard.java
+++ src/main/java/si/dejan/scoreboard/Scoreboard.java
@@ -6,6 +6,11 @@ public class Scoreboard {
     }
 
     public void startMatch(Match match) {
+        // validate
+        if (match == null) {
+            throw new NullPointerException("Match must be set!");
+        }
+        
         // todo optimise this to call method in match to set scores to 0
         match.getHomeTeam().setScore(0);
         match.getAwayTeam().setScore(0);
