4e76cf1cee9b69f6de7df1295fa4da09be650b4e
Dejan Pirc <ddeanp@gmail.com>
2024-11-24 21:57:26 +0100

    UPDATE: MatchTest.java and TeamTest.java code cleanup

--- src/test/java/si/dejan/scoreboard/MatchTest.java
+++ src/test/java/si/dejan/scoreboard/MatchTest.java
@@ -6,30 +6,29 @@ import static org.junit.Assert.assertThrows;
 import org.junit.Test;
 
 public class MatchTest {
-@Test
-    public void newMatch_onlyHomeTeamIsSet_throwsException(){
+
+    private void assertNoHomeAwayTeam(Team homeTeam, Team awayTeam) {
+        Exception exception = assertThrows(Exception.class, () -> new Match(homeTeam, awayTeam));
+
+        assertEquals("Home and away teams must be set!", exception.getMessage());
+    }
+
+    @Test
+    public void newMatch_onlyHomeTeamIsSet_throwsException() {
         Team homeTeam = new Team("Mexico");
-        
-        Exception exception = assertThrows(Exception.class, () -> {Match match = new Match(homeTeam, null);});
-        
-        assertEquals("Home and away teams must be set!", exception.getMessage());
+
+        assertNoHomeAwayTeam(homeTeam, null);
     }
 
     @Test
-    public void newMatch_onlyAwayTeamIsSet_throwsException(){
+    public void newMatch_onlyAwayTeamIsSet_throwsException() {
         Team awayTeam = new Team("Canada");
-        
-        Exception exception = assertThrows(Exception.class, () -> {Match match = new Match(null, awayTeam);});
-        
-        assertEquals("Home and away teams must be set!", exception.getMessage());
+
+        assertNoHomeAwayTeam(null, awayTeam);
     }
 
     @Test
-    public void newMatch_noTeamIsSet_throwsException(){
-        Exception exception = assertThrows(Exception.class, () -> {
-            Match match = new Match(null, null);
-        });
-
-        assertEquals("Home and away teams must be set!", exception.getMessage());
+    public void newMatch_noTeamIsSet_throwsException() {
+        assertNoHomeAwayTeam(null, null);
     }
 }
--- src/test/java/si/dejan/scoreboard/TeamTest.java
+++ src/test/java/si/dejan/scoreboard/TeamTest.java
@@ -6,32 +6,32 @@ import static org.junit.Assert.assertThrows;
 import org.junit.Test;
 
 public class TeamTest {
-    
+
     private void assertInvalidTeamName(String name) {
-        Exception exception = assertThrows(Exception.class, () -> { Team team = new Team(name); });
-        
+        Exception exception = assertThrows(Exception.class, () -> new Team(name));
+
         assertEquals("Invalid team name!", exception.getMessage());
     }
 
     @Test
-    public void newTeam_teamNameIsNull_throwsException(){
-        Exception exception = assertThrows(Exception.class, () -> { Team team = new Team(null); });
-        
+    public void newTeam_teamNameIsNull_throwsException() {
+        Exception exception = assertThrows(Exception.class, () -> new Team(null));
+
         assertEquals("Name must be set!", exception.getMessage());
     }
 
     @Test
-    public void newTeam_teamNameContainsNumber_throwsException(){
+    public void newTeam_teamNameContainsNumber_throwsException() {
         assertInvalidTeamName("Team1");
     }
 
     @Test
-    public void newTeam_teamNameContainsSymbol_throwsException(){
+    public void newTeam_teamNameContainsSymbol_throwsException() {
         assertInvalidTeamName("Team!");
     }
 
     @Test
-    public void newTeam_teamNameContainsForeignLetters_createsTeamClass(){
+    public void newTeam_teamNameContainsForeignLetters_createsTeamClass() {
         String name = "Teamščžüöäëíí";
 
         Team team = new Team(name);
